2b5625b2a525b3df0727f6c2ea12b2aa324022b0
Sivarjuen Ravichandran <sivarjuen.ravichandran@hotmail.com>
2020-02-14 18:04:50 +0000

    Add knight sprite, character movement and rotation

--- scripts/characters/character.gd
+++ scripts/characters/character.gd
@@ -1,20 +1,79 @@
 extends Node2D
 
+enum Direction{
+	North,
+	South,
+	East,
+	West
+}
+
 var green_cell = preload("res://map/CellHighlight.tscn")
 
 var selected = false
-var movement = 2
+var movement = 4
 var current_position;
 var target_position;
 var under_mouse = false;
 var highlighted_cells = [];
 var global_cancel_origin = false
+var facing = Direction.South
+
+var move_speed = 300
+var turn_speed = 1
+var turning_to;
+var movement_vec = Vector2(0,0)
+var next_cell;
+var final_cell;
+var moving = false
 
 onready var Grid = get_node("/root/Game/Map/Grid")
+onready var character_sprite = get_node("CharacterSprite")
 
 func _ready():
 	add_to_group("characters")
 	current_position = Grid.world_to_map(global_position)
+	
+func _process(delta):
+	if moving:
+		if in_range(next_cell):
+			movement_vec = Vector2(0,0)
+			if next_cell == final_cell:
+				moving = false
+			else:
+				pick_next_cell()
+		else:
+			set_global_position(get_global_position() + movement_vec * move_speed * delta)
+					
+
+func pick_next_cell():
+	var remaining = final_cell - current_position
+	if remaining.x > 0: # move right
+		next_cell = current_position + Vector2(1,0)
+		facing = Direction.East
+		character_sprite.set_global_rotation_degrees(-90)
+	elif remaining.x < 0: # move left
+		next_cell = current_position + Vector2(-1,0)
+		facing = Direction.West
+		character_sprite.set_global_rotation_degrees(90)
+	elif remaining.y > 0: # move down
+		next_cell = current_position + Vector2(0,1)
+		facing = Direction.South
+		character_sprite.set_global_rotation_degrees(0)
+	elif remaining.y < 0: # move up
+		next_cell = current_position + Vector2(0,-1)
+		facing = Direction.North
+		character_sprite.set_global_rotation_degrees(180)
+	movement_vec = next_cell - current_position
+	
+func in_range(cell):
+	var target = Grid.map_to_world_fixed(cell)
+	var offset = target - global_position
+	var passed = false
+	if (facing == Direction.North and offset.y > 0) or (facing == Direction.South and offset.y < 0) or (facing == Direction.East and offset.x < 0) or (facing == Direction.West and offset.x > 0):
+		set_global_position(target)
+		current_position = cell
+		return true
+	return false
 	
 func select():
 	selected = true
@@ -36,8 +95,9 @@ func act():
 	cancel()
 	
 func move_to(cell):
-	set_global_position(Grid.map_to_world_fixed(cell))
-	current_position = cell
+	final_cell = cell
+	pick_next_cell()
+	moving = true
 
 func _on_SelectZone_mouse_entered():
 	under_mouse = true
@@ -75,3 +135,7 @@ func valid_movement_target(target):
 		if target == cell_position:
 			return true
 	return false
+	
+func determine_direction(target):
+	var dx = target.x - current_position.x
+	var dy = target.y - current_position.y
